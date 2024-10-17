ba2c57c06fd771721f45f28cd670afef844078a6
Rayz5103 <tuanray123@gmail.com>
2024-10-17 08:20:32 +0700

    first commit

---
+++ .history/productform/lib/home_screen_20241009210738.dart
@@ -0,0 +1,178 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _categoryController = TextEditingController();
+  final TextEditingController _priceController = TextEditingController();
+
+  CollectionReference products =
+      FirebaseFirestore.instance.collection('product');
+      void _addProduct() {
+        products.add({
+          'Name': _nameController.text,
+          'Category': _categoryController.text,
+          'Price': _priceController.text,
+        });
+        _nameController.clear();
+        _categoryController.clear();
+        _priceController.clear();
+      }
+
+      void _deleteProduct(String productId) {
+        products.doc(productId).delete();
+      }
+
+      void _editProduct(DocumentSnapshot product) {
+        _nameController.text = product['Name'];
+        _categoryController.text = product['Category'];
+        _priceController.text = product['Price'];
+
+        showDialog(context: context, builder: (context) {
+          return AlertDialog(
+            title: Text('Edit product'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextFormField(
+                  controller: _nameController,
+                  decoration: InputDecoration(labelText: 'Product Name'),
+                ),
+                SizedBox(height: 8),
+                TextFormField(
+                  controller: _categoryController,
+                  decoration: InputDecoration(labelText: 'Category Name'),
+                ),
+                 SizedBox(height: 8),
+                TextFormField(
+                  controller: _priceController,
+                  decoration: InputDecoration(labelText: 'Price'),
+                ),
+              ],
+            ),
+            actions: [
+              TextButton(onPressed: (){
+                Navigator.pop(context);
+              }, child: Text('Cancel')),
+              ElevatedButton(onPressed: () {
+                _updateProduct(product.id);
+              }, child: Text('Update')),
+            ],
+          );
+        });
+      }
+
+      void _updateProduct(String productId) {
+        products.doc(productId).update({
+          'Name': _nameController.text,
+          'Category': _categoryController.text,
+          'Price': _priceController.text,
+        }
+        );
+      }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("DANH SÁCH SẢN PHẨM"),
+      ),
+      body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              TextField(
+                controller: _nameController,
+                decoration: InputDecoration(labelText: 'Tên sản phẩm'),
+              ),
+              SizedBox(height: 16),
+              TextField(
+                controller: _categoryController,
+                decoration: InputDecoration(labelText: 'Loại sản phẩm'),
+              ),
+              SizedBox(height: 16),
+              TextField(
+                controller: _priceController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(labelText: 'Giá sản phẩm'),
+              ),
+              SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  _addProduct();
+                },
+                child: Text("Thêm sản phẩm"),
+              ),
+              SizedBox(height: 16),
+              Expanded(
+                  child: StreamBuilder(
+                stream: products.snapshots(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return CircularProgressIndicator();
+                  }
+                  return ListView.builder(
+                    itemCount: snapshot.data!.docs.length,
+                    itemBuilder: (context, index) {
+                      var product = snapshot.data!.docs[index];
+                      return Dismissible(
+                        key: Key(product.id),
+                        background: Container(
+                          color: Colors.redAccent,
+                          alignment: Alignment.centerRight,
+                          padding: EdgeInsets.only(right: 16),
+                          child: Icon(Icons.delete,
+                          color: Colors.white,)
+                        ),
+                        onDismissed: (direction){
+                          _deleteProduct(product.id);
+                        },
+                        direction: DismissDirection.endToStart,
+                        child: Card(
+                            elevation: 4,
+                            margin: EdgeInsets.symmetric(vertical: 8),
+                            child: ListTile(
+                              title: Text('Tên sp: ${product['Name']}'),   
+                              subtitle: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                  Text('Chung sp: ${product['Category']}'),
+                                      Text('Giá sp: ${product['Price']}'),
+                                  ],
+                                    
+                                  
+                              ),
+                            trailing: Column(
+                              children: [
+                                IconButton(
+                                  onPressed: () {
+                                    _editProduct(product);
+                                  },
+                                  icon: Icon(Icons.edit),
+                                ),
+                              ],
+                            ),
+                            ),
+                        
+                        ),
+                      );
+                    },
+                  );
+                },
+              )),
+            ],
+          )),
+    );
+  }
+}
+
+extension on Object {
+  get docs => null;
+}
---
+++ .history/productform/lib/main_20241009172730.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:productform/home_screen.dart';
+import 'firebase_options.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
+runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      title: 'PRODUCT_FORM',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.grey[200],
+        primaryColor: Colors.blueAccent,
+        appBarTheme: AppBarTheme(
+          color: Colors.pink,
+        ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.yellow,
+          foregroundColor: Colors.black,
+        ),
+      ),
+      inputDecorationTheme: InputDecorationTheme(
+        border: OutlineInputBorder(),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.blue,
+          ),
+        )
+      )
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
